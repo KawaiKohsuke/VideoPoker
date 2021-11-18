93d8c1314fc6e490567d1bfe06a97c5718ed0959
KawaiKohsuke <chiyo_kakitome@yahoo.co.jp>
2021-11-18 14:47:37 +0900

    Update videopoker.js

--- videopoker.js
+++ videopoker.js
@@ -1,20 +1,31 @@
 let cards = new Array(53);
+let new_cards = new Array(53);
 
-for (let i=0; i<53; i++) {
-    cards[i] = i;
+
+for (let i=0; i<13; i++) {
+    cards[i] = {suit: "spade", rank: i + 1};
+    cards[i+13] = {suit: "club", rank: i + 1};
+    cards[i+26] = {suit: "diamond", rank: i + 1};
+    cards[i+39] = {suit: "heart", rank: i + 1};
+}
+cards[52] = {suit: "joker", rank: -1};
+
+
+
+
+new_cards = shuffle(cards);
+
+
+
+for (let i=0; i<5; i++) {
+    console.log(i+1 + "枚目 -> " + new_cards[i].suit + " " + new_cards[i].rank);
 }
 
-cards = shuffle(cards);
-
-
-
-for (let i=0; i<53; i++) {
-    console.log(cards[i]);
-}
 
 
 function shuffle(arr) {
     for (let i=arr.length-1; i>0; i--) {
+        
         let j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
